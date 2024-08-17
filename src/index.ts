4b841f996b533bd3b139fac26051fe3268c34a59
Lala Sabathil <aiko@aitsys.dev>
2024-08-17 13:59:15 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
-try {
-  const allowedIds = core.getInput('whitelisted-github-ids');
-  if (allowedIds === undefined || allowedIds === "") {
-    throw new Error("Input 'whitelisted-github-ids' was empty.")
+async function run() {
+  try {
+    const allowedIds = core.getInput('whitelisted-github-ids');
+    if (allowedIds === undefined || allowedIds === "") {
+      throw new Error("Input 'whitelisted-github-ids' was empty.")
+    }
+
+    const allowedUserIds = allowedIds.split(',');
+
+    const octokit = github.getOctokit(core.getInput('GITHUB_TOKEN'));
+
+    const username = github.context.actor;
+
+    const { data: user } = await octokit.rest.users.getByUsername({
+      username: username,
+    });
+
+    const userId = user.id.toString();
+
+    if (allowedUserIds.includes(userId)) {
+      console.log(`User ${userId} is allowed to run this workflow.`);
+    } else {
+      throw new Error(`User ${userId} is not authorized to run this workflow.`);
+    }
+  } catch (error: any) {
+    core.setFailed(error.message);
   }
-
-  const allowedUserIds = allowedIds.split(',');
-
-  const userId = github.context.actor;
-
-  if (allowedUserIds.includes(userId)) {
-    console.log(`User ${userId} is allowed to run this workflow.`);
-  } else {
-    throw new Error(`User ${userId} is not authorized to run this workflow.`);
-  }
-} catch (error: any) {
-  core.setFailed(error.message);
 }
+
+run();
